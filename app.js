8a8c4290f77c75a654d9e1e0facb78a6c08dbcc6
cortezalberto <cortezalberto@gmail.com>
2023-01-18 12:36:49 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// requerimos express y path
+const express = require('express');
+const path = require('path');
+
+// guardamos en un constante app la funcionalidad de express()
+const app = express();
+
+// definimos el puerto 
+const port = 3030;
+
+// definimos las rutas para los archivos estáticos(públicos) y otra para las vistas
+const viewsPath = path.resolve(__dirname, 'views');
+const publicPath = path.resolve(__dirname, 'public');
+
+// Agregamos el middleware para configurar la carpeta de archivos estáticos
+app.use(express.static(publicPath));
+
+// Definimos las rutas que responda a GET
+app.get('/', (req, res) => res.sendFile(path.resolve(viewsPath, 'home.html')));
+
+app.get('/register', (req, res) => res.sendFile(path.resolve(viewsPath, 'register.html')));
+app.get('/login', (req, res) => res.sendFile(path.resolve(viewsPath, 'login.html')));
+
+// Levantamos el servidor
+app.listen(process.env.PORT || port, () => console.log(`Server running`));
